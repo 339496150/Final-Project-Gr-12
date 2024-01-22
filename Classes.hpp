3d4225163d777b6bbc960ff71f31239926bf5992
339496150 <339496150@gotvdsb.ca>
2024-01-22 13:37:30 -0500

    balckjack is in classes

--- Classes.hpp
+++ Classes.hpp
@@ -64,6 +64,87 @@ class MiniBoss
     {
         return dragonKilled;
     }
+
+    void dragonBlackjack(Hero &hero) 
+    {
+        char choice;
+        cin.ignore(256, '\n');
+        Deck deck;
+        Player player;
+        Player dealer;
+
+        //First deal
+        player.addCard(deck.dealCard());
+        dealer.addCard(deck.dealCard());
+        player.addCard(deck.dealCard());
+        dealer.addCard(deck.dealCard());
+
+        //Player turn
+        cout << "Your hand:" << endl;
+        player.displayHand();
+
+
+        while (choice != 's')
+        {
+            cout << "Do you want to hit (h) or stand (s)? ";
+            cin >> choice;
+
+            if (choice == 'h') 
+            {
+                player.addCard(deck.dealCard());
+                cout << "Your hand:" << endl;
+                player.displayHand();
+
+                if (player.getTotalScore() > 21) 
+                {
+                    cout << "You missed" << endl;
+                    cout << "Sami's turn!" << endl;
+                    cout << "Sami attacked you for 20 damage" << endl;
+                    hero.setHealth(hero.getHealth() - damage);
+                    break;
+                }
+            }
+
+        }
+
+        //Dealer turn
+        cout << "Dealer's hand:" << endl;
+        dealer.displayHand();
+
+        while (dealer.getTotalScore() < 17) 
+        {
+            dealer.addCard(deck.dealCard());
+            cout << "Dealer hit" << endl;
+        }
+
+        cout << "Dealer's hand:" << endl;
+        dealer.displayHand();
+
+        //Check for win
+        if (dealer.getTotalScore() > 21 || (player.getTotalScore() <= 21 && player.getTotalScore() > dealer.getTotalScore())) 
+        {
+           cout << "You managed to hit Sami" << endl;
+            health -= hero.getDamage();
+            if (health <= 0) 
+            {
+                dragonDead(true);
+                cout << "You have killed Sami." << endl;
+                cout << "You feel more confident after killing the dragon" << endl;
+                cout << "Your new attack strength is 30" << endl;
+                hero.setDamage(30);
+            }
+            cout << "Sami's turn!" << endl;
+            cout << "Sami attacked you for 20 damage" << endl;
+            hero.setHealth(hero.getHealth() - damage);
+        } 
+        else 
+        {
+            cout << "You missed" << endl;
+            cout << "Sami's turn!" << endl;
+            cout << "Sami attacked you for 20 damage" << endl;
+            hero.setHealth(hero.getHealth() - damage);
+        }
+    }
 };
 
 class Boss
@@ -84,6 +165,81 @@ class Boss
     {
         return cthulhuKilled;
     }
+
+    void bossBlackjack(Hero &hero) 
+    {
+        char choice;
+        cin.ignore(256, '\n');
+        Deck deck;
+        Player player;
+        Player dealer;
+
+        //First deal
+        player.addCard(deck.dealCard());
+        dealer.addCard(deck.dealCard());
+        player.addCard(deck.dealCard());
+        dealer.addCard(deck.dealCard());
+
+        //Player turn
+        cout << "Your hand:" << endl;
+        player.displayHand();
+
+
+        while (choice != 's')
+        {
+            cout << "Do you want to hit (h) or stand (s)? ";
+            cin >> choice;
+
+            if (choice == 'h') 
+            {
+                player.addCard(deck.dealCard());
+                cout << "Your hand:" << endl;
+                player.displayHand();
+
+                if (player.getTotalScore() > 21) 
+                {
+                    cout << "You missed" << endl;
+                    cout << "Cthulhu's turn!" << endl;
+                    cout << "Cthulhu attacked you for 25 damage" << endl;
+                    hero.setHealth(hero.getHealth() - damage);
+                    heroKilled(hero);
+                    break;
+                }
+            }
+
+        }
+
+        //Dealer turn
+        cout << "Dealer's hand:" << endl;
+        dealer.displayHand();
+
+        while (dealer.getTotalScore() < 17) 
+        {
+            dealer.addCard(deck.dealCard());
+            cout << "Dealer hit" << endl;
+        }
+
+        cout << "Dealer's hand:" << endl;
+        dealer.displayHand();
+
+        //Check for win
+        if (dealer.getTotalScore() > 21 || (player.getTotalScore() <= 21 && player.getTotalScore() > dealer.getTotalScore())) 
+        {
+            cout << "You hit Cthulhu" << endl;
+            health -= hero.getDamage();
+            if (health <= 0)
+            {
+                cthulhuDead(true);
+            }
+        } 
+        else 
+        {
+            cout << "You missed" << endl;
+            cout << "Cthulhu's turn!" << endl;
+            cout << "Cthulhu attacked you for 25 damage" << endl;
+            hero.setHealth(hero.getHealth() - damage);
+        }
+    }
 };
 
 class BaseEnemy
@@ -103,5 +259,76 @@ class BaseEnemy
     bool killedEnemy()
     {
         return enemyKilled;
+    }
+
+    void ghostBlackjack(Hero &hero) 
+    {
+        char choice;
+        cin.ignore(256, '\n');
+        Deck deck;
+        Player player;
+        Player dealer;
+
+        //First deal
+        player.addCard(deck.dealCard());
+        dealer.addCard(deck.dealCard());
+        player.addCard(deck.dealCard());
+        dealer.addCard(deck.dealCard());
+
+        //Player turn
+        cout << "Your hand:" << endl;
+        player.displayHand();
+
+
+        while (choice != 's')
+        {
+            cout << "Do you want to hit (h) or stand (s)? ";
+            cin >> choice;
+
+            if (choice == 'h') 
+            {
+                player.addCard(deck.dealCard());
+                cout << "Your hand:" << endl;
+                player.displayHand();
+
+                if (player.getTotalScore() > 21) 
+                {
+                    cout << "You missed" << endl;
+                    cout << "The ghost attacked you for 5 damage" << endl;
+                    hero.setHealth(hero.getHealth() - damage);
+                    break;
+                }
+            }
+
+        }
+
+        //Dealer turn
+        cout << "Dealer's hand:" << endl;
+        dealer.displayHand();
+
+        while (dealer.getTotalScore() < 17) 
+        {
+            dealer.addCard(deck.dealCard());
+            cout << "Dealer hit" << endl;
+        }
+
+        cout << "Dealer's hand:" << endl;
+        dealer.displayHand();
+
+        //Check for win
+        if (dealer.getTotalScore() > 21 || (player.getTotalScore() <= 21 && player.getTotalScore() > dealer.getTotalScore())) 
+        {
+            cout << "you managed to get a hit on the enemy" << endl;
+                health -= hero.getDamage();
+                cout << "You killed the ghost" << endl;
+                enemyDead(true);
+        } 
+        else 
+        {
+            cout << "You missed" << endl;
+            cout << "The ghost attacked you for 5 damage" << endl;
+            hero.setHealth(hero.getHealth() - damage);
+            heroKilled(hero);
+        }
     }
 };
